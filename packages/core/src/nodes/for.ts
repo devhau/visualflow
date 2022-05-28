a932dc0dd7306a705c7605acf6ea01452eac12a0
Dev Hau <50167033+devhau@users.noreply.github.com>
2022-05-29 06:59:29 +0700

    Update for.ts

--- packages/core/src/nodes/for.ts
+++ packages/core/src/nodes/for.ts
@@ -11,8 +11,39 @@ export class CoreForNode extends WorkerNode {
   icon() {
     return '<i class="fas fa-long-arrow-alt-right"></i>';
   }
+  properties(): any {
+    return {
+      number_start: {
+        key: "number_start",
+        edit: true,
+        default: 1
+      },
+      number_end: {
+        key: "number_end",
+        edit: true,
+        default: 1
+      },
+      number_step: {
+        key: "number_step",
+        edit: true,
+        default: 1
+      },
+      env_name: {
+        key: "env_name",
+        edit: true,
+        default: 'loop_index'
+      }
+    }
+  }
 
   async execute(nodeId: any, data: any, manager: WorkerManager, next: any) {
+    const group = manager.getGroupCurrent();
+    manager.setGroup(data.id);
+    for (let loop_index = data.number_start; loop_index < data.number_end; loop_index = loop_index + data.number_step) {
+
+      await manager.excuteAsync();
+    }
+    manager.setGroup(group);
     await this.nextNode(data, next, nodeId);
   }
 }
